23a82d2363cf619785a5a1580e5974f038fdfbf2
adminabhangadvt <adminabhangadvt@gmail.com>
2026-07-05 14:30:22 +0530

    jenkinesfile updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send(`
         <h1>🚀 Welcome to My First DevOps Project!</h1>
-        <h1>My 1st Change</h1>
+        <h1>🚀 Version 2 Deployed Automatically!</h1>
         <p>This app will soon run inside Docker and be deployed using Jenkins.</p>
     `);
 });
